8210797e31cf2b360a5519cadfb5480ba072ad8e
Mads505m <onlinebigchungus@gmail.com>
2024-02-26 12:05:25 +0100

    start

--- src/main/java/com/example/semesterprojekt2/HelloController.java
+++ src/main/java/com/example/semesterprojekt2/HelloController.java
@@ -3,12 +3,27 @@ package com.example.semesterprojekt2;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.InputStream;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private ImageView Logo;
+
+
+
+    @FXML
+    protected void onHelloButtonClick1() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onHelloButtonClick2() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
