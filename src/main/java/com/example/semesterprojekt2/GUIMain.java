4c20fea71b2ce0ede8e6d0317908a1531e7d7059
Mads505m <onlinebigchungus@gmail.com>
2024-03-01 00:47:54 +0100

    Ikon

--- src/main/java/com/example/semesterprojekt2/GUIMain.java
+++ src/main/java/com/example/semesterprojekt2/GUIMain.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.image.Image;
+import java.awt.*;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class GUIMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUIMain.class.getResource("StartScreen/OpeningScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 389, 682);
-        stage.setTitle("Hello!");
+        stage.setTitle("MonkasBarberShop");
+        InputStream iconStream = getClass().getResourceAsStream("/Pictures/Frisør-removebg-preview.png");
+        Image image = new Image(iconStream);
+        stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
     }
