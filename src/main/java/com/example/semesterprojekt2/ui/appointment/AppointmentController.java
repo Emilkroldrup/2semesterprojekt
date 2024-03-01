325a4019e5cde76c9635e7d26458465568bfa580
minkguel <mikkel.ostvand.pedersen@gmail.com>
2024-03-01 11:36:31 +0100

    small changes to appoint/treatments fxml

--- src/main/java/com/example/semesterprojekt2/ui/appointment/AppointmentController.java
+++ src/main/java/com/example/semesterprojekt2/ui/appointment/AppointmentController.java
@@ -1,5 +1,50 @@
 package com.example.semesterprojekt2.ui.appointment;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class AppointmentController {
-    
+
+    @FXML
+    private Label titleLabel = new Label();
+
+    @FXML
+    private DatePicker datePicker = new DatePicker();
+
+    @FXML
+    private Parent root;
+
+    @FXML
+    private Stage stage;
+
+    @FXML
+    private Scene scene;
+
+    @FXML
+    protected void SwitchSceneToAppointment(javafx.event.ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AppointmentView.fxml"));
+        stage= (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void chooseDate () {
+
+        datePicker.getOnAction(); // gets clicked date
+    }
+
 }
--- src/main/java/com/example/semesterprojekt2/ui/treatment/TreatmentController.java
+++ src/main/java/com/example/semesterprojekt2/ui/treatment/TreatmentController.java
@@ -1,5 +1,26 @@
 package com.example.semesterprojekt2.ui.treatment;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
 public class TreatmentController {
-    
+
+    @FXML
+    Label treatmentsLabel = new Label (); // title
+    @FXML
+    Button trimButton = new Button(); // opens booking scene
+    @FXML
+    Button hairdyeButton = new Button(); // opens booking scene
+    @FXML
+    Button hairwashButton = new Button(); // opens booking scene
+    @FXML
+    Button shaveButton = new Button(); // opens booking scene
+    @FXML
+    Button requestButton = new Button(); // opens a text editor for the customer to request a custom treatment
+    @FXML
+    Button returnButton = new Button(); // returns to former scene
+
+
 }
