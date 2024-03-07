8b7a358a45097d8d2b84fd2068e45d747a51e31c
minkguel <146935836+minkguel@users.noreply.github.com>
2024-03-07 18:15:07 +0100

    merge

--- src/main/java/com/example/semesterprojekt2/ui/appointment/AppointmentController.java
+++ src/main/java/com/example/semesterprojekt2/ui/appointment/AppointmentController.java
@@ -10,17 +10,10 @@ import com.example.semesterprojekt2.ui.login.LoginControllerCustomer;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -43,14 +36,6 @@ public class AppointmentController {
 
     @FXML
     private Button bookingButton = new Button();
-    private Parent root;
-
-    @FXML
-    private Stage stage;
-
-    @FXML
-    private Scene scene;
-
 
     private AppointmentService appointmentService;
 
@@ -128,17 +113,6 @@ public class AppointmentController {
         
     }
     */
-
-
-
-    @FXML
-    protected void backToLogin(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/com/example/semesterprojekt2/StartScreen/OpeningScene.fxml"));
-        stage= (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
 
 
 
